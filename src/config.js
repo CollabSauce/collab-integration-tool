45980c929bc00f0f85234b90e0e17ba4d3a2dcb4
Brandon Cooper <cooperjbrandon@gmail.com>
2020-08-19 15:04:58 -0700

    much more prog

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+import { parseBoxShadow } from 'src/utils/boxShadow';
+
 export const WIDGET_TYPES = {
   SELECT: 'select',
   COLOR: 'color',
@@ -127,4 +129,236 @@ export const STYLE_CONFIG = {
     selectOptions: BORDER_STYLE_OPTIONS,
     customSetAttribute: 'setBorderAttributes'
   },
+  borderAllRadius: {
+    default: 0,
+    label: 'Border Radius: All Corners:',
+    parsePx: false,
+    widget: WIDGET_TYPES.SLIDER,
+    additionalKeysToUpdate: ['borderTopLeftRadius','borderTopRightRadius','borderBottomRightRadius','borderBottomLeftRadius'],
+    customSetAttribute: 'setBorderRadiusAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  borderTopLeftRadius: {
+    default: 0,
+    label: 'Border Radius Top Left',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setBorderRadiusAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  borderTopRightRadius: {
+    default: 0,
+    label: 'Border Radius Top Right',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setBorderRadiusAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  borderBottomRightRadius: {
+    default: 0,
+    label: 'Border Radius Bottom Right',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setBorderRadiusAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  borderBottomLeftRadius: {
+    default: 0,
+    label: 'Border Radius Bottom Left',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setBorderRadiusAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  marginAll: {
+    default: 0,
+    label: 'Margin All:',
+    parsePx: false,
+    widget: WIDGET_TYPES.SLIDER,
+    additionalKeysToUpdate: ['marginTop','marginRight','marginBottom','marginLeft'],
+    customSetAttribute: 'setMarginAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  marginTop: {
+    default: 0,
+    label: 'Margin Top:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setMarginAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  marginRight: {
+    default: 0,
+    label: 'Margin Right:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setMarginAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  marginBottom: {
+    default: 0,
+    label: 'Margin Bottom:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setMarginAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  marginLeft: {
+    default: 0,
+    label: 'Margin Left:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setMarginAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  paddingAll: {
+    default: 0,
+    label: 'Padding All:',
+    parsePx: false,
+    widget: WIDGET_TYPES.SLIDER,
+    additionalKeysToUpdate: ['paddingTop','paddingRight','paddingBottom','paddingLeft'],
+    customSetAttribute: 'setPaddingAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  paddingTop: {
+    default: 0,
+    label: 'Padding Top:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setPaddingAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  paddingRight: {
+    default: 0,
+    label: 'Padding Right:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setPaddingAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  paddingBottom: {
+    default: 0,
+    label: 'Padding Bottom:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setPaddingAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  paddingLeft: {
+    default: 0,
+    label: 'Padding Left:',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customSetAttribute: 'setPaddingAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  boxShadowHorizontalOffset: {
+    default: 0,
+    label: 'Box Shadow Horizontal Offset',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customParseFn: (val) => { return parseBoxShadow(val)[0].offsetX },
+    customParseFnKey: 'boxShadow',
+    customSetAttribute: 'setBoxShadowAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  boxShadowVerticalOffset: {
+    default: 0,
+    label: 'Box Shadow Vertical Offset',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customParseFn: (val) => { return parseBoxShadow(val)[0].offsetY },
+    customParseFnKey: 'boxShadow',
+    customSetAttribute: 'setBoxShadowAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  boxShadowBlurRadius: {
+    default: 0,
+    label: 'Box Shadow Blur Radius',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customParseFn: (val) => { return parseBoxShadow(val)[0].blurRadius },
+    customParseFnKey: 'boxShadow',
+    customSetAttribute: 'setBoxShadowAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  boxShadowSpreadRadius: {
+    default: 0,
+    label: 'Box Shadow Spread Radius',
+    parsePx: true,
+    widget: WIDGET_TYPES.SLIDER,
+    customParseFn: (val) => { return parseBoxShadow(val)[0].spreadRadius },
+    customParseFnKey: 'boxShadow',
+    customSetAttribute: 'setBoxShadowAttributes',
+    widgetProps: {
+      min: 0,
+      max: 200,
+    }
+  },
+  boxShadowColor: {
+    default: '',
+    label: 'Box Shadoww Color:',
+    parsePx: false,
+    widget: WIDGET_TYPES.COLOR,
+    customParseFn: (val) => { return parseBoxShadow(val)[0].color },
+    customParseFnKey: 'boxShadow',
+    customSetAttribute: 'setBoxShadowAttributes',
+  },
+//   boxShadowInsetOutset: {
+//
+//   },
 };
