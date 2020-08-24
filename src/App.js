6bdb0d0aceeb26e76646c8fa2cdeec18aab3c5c4
Brandon Cooper <cooperjbrandon@gmail.com>
2020-08-23 20:26:38 -0700

    rename to Toolbar. add polyfills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 import './App.css';
-import { Toolbar } from 'src/components/DraggableToolbar';
+
+import 'react-app-polyfill/ie11';
+// DO I NEED THESE?
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
+import 'core-js/features/url/index';
+import 'core-js/features/url/to-json';
+import 'core-js/features/url-search-params/index';
+import "core-js/modules/es.object.from-entries";
+
+import { Toolbar } from 'src/components/Toolbar';
 
 function App() {
   return (
