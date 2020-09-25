9f6452027c5e273fd1cf048753a0c1a9b68e1ee2
Brandon Cooper <cooperjbrandon@gmail.com>
2020-09-25 15:46:54 -0700

    different env vars for different stages

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,13 @@
 export const API_PATH = '/api';
 export const AUTH_TOKEN_KEY = 'auth_token';
-export const BASE_PATH = 'http://localhost:8000';
+
+let basePath = '';
+if (process.env.REACT_APP_ENV === 'development') {
+  basePath = 'http://localhost:8000';
+} else if (process.env.REACT_APP_ENV === 'staging') {
+  basePath = 'https://pacific-earth-58699.herokuapp.com';
+} else if (process.env.REACT_APP_ENV === 'production') {
+  basePath = 'https://api.collabsauce.com';
+}
+
+export const BASE_PATH = basePath;
