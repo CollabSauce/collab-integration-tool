09f1cea6131f9fca27b97e3736bbf2eb9b972f15
Brandon Cooper <cooperjbrandon@gmail.com>
2020-09-29 09:49:48 -0700

    show url origin in task metadata

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -113,6 +113,7 @@ const TaskDetail = () => {
     }
     const {
       created,
+      urlOrigin,
       browserName,
       browserVersion,
       osName,
@@ -129,6 +130,11 @@ const TaskDetail = () => {
       {
         key: 'Task Created At',
         value: dayjs(created).format('dddd, MMMM D, YYYY h:mm A'),
+      },
+      {
+        key: 'Url',
+        value: urlOrigin,
+        link: true,
       },
       {
         key: 'Browser',
@@ -250,10 +256,16 @@ const TaskDetail = () => {
           </CollapseHeader>
           <Collapse isOpen={openCollapsibleStates.metadata} className="ml-1">
             <ListGroup flush>
-              {metadatas.map(({ key, value }) => (
+              {metadatas.map(({ key, value, link }) => (
                 <ListGroupItem key={key} className="text-sans-serif fs--1 p-2 background-color-inherit">
                   <p className="mb-1 font-weight-semi-bold">{key}</p>
-                  <p className="mb-0">{value}</p>
+                  {link ? (
+                    <a href={value} target="_blank" rel="noopener noreferrer" className="color-inherit">
+                      {value}
+                    </a>
+                  ) : (
+                    <p className="mb-0">{value}</p>
+                  )}
                 </ListGroupItem>
               ))}
             </ListGroup>
