c3eb5909327e57213406589270b9bc7bdeca5d48
Brandon Cooper <cooperjbrandon@gmail.com>
2020-10-11 21:57:59 -0700

    dont build twice. make everything sync. manually remove sourcemaps after upload

--- build_scripts/build.js
+++ build_scripts/build.js
@@ -7,109 +7,79 @@ const SentryCli = require('@sentry/cli');
 const RELEASE = shell.exec('git rev-parse --short HEAD').replace(/(\r\n|\n|\r)/gm, ''); // Strip away the \n of the `release`.
 const BASE_BUILD_COMMAND = `REACT_APP_SENTRY_RELEASE=${RELEASE} REACT_APP_ENV=${process.env.REACT_APP_ENV} react-scripts build`;
 
-function findHashFromFileName(startPath, filter, onHashesFound) {
-  if (!fs.existsSync(startPath)) {
-    console.log(chalk.red(`no dir ${startPath}`));
-    return;
-  }
-
-  const files = fs.readdirSync(startPath);
-  let hashes = [];
-  for (let i = 0; i < files.length; i++) {
-    const filename = path.join(startPath, files[i]);
-    const stat = fs.lstatSync(filename);
-    if (filter.test(filename)) {
-      const hash = filename.match(filter)[1];
-      hashes.push(hash);
+function removeSourceMapsForDirectory(startPath, filters) {
+  for (let i = 0; i < filters.length; i++) {
+    const filter = filters[i];
+    if (!fs.existsSync(startPath)) {
+      console.log(chalk.red(`no dir ${startPath}`));
+      return;
     }
-  }
 
-  if (hashes.length !== 1) {
-    console.log(chalk.red(`ERROR: expected exactly 1 hash for ${filter} but got ${hashes.length}`));
-    console.log(chalk.red(`BUILD IS WRONG. DO NOT DEPLOY`));
-  }
-  onHashesFound(hashes[0]);
-}
+    const files = fs.readdirSync(startPath);
+    let matchedFilenames = [];
+    for (let i = 0; i < files.length; i++) {
+      const filename = path.join(startPath, files[i]);
+      const stat = fs.lstatSync(filename);
+      if (filter.test(filename)) {
+        matchedFilenames.push(filename);
+      }
+    }
 
-function getOutputHashes(onGotHashes) {
-  const directory = 'build/static/js';
-  let appHash = '';
-  let chunkHash = '';
-  let runtimeMainHash = '';
-  findHashFromFileName(directory, /build\/static\/js\/main\.(.*)\.chunk\.js$/, function (hashapp) {
-    appHash = hashapp;
-    findHashFromFileName(directory, /build\/static\/js\/2\.(.*)\.chunk\.js$/, function (hashchunk) {
-      chunkHash = hashchunk;
-      findHashFromFileName(directory, /build\/static\/js\/runtime-main\.(.*)\.js$/, function (hashRuntimemain) {
-        runtimeMainHash = hashRuntimemain;
-        onGotHashes(appHash, chunkHash, runtimeMainHash);
-      });
-    });
-  });
+    if (matchedFilenames.length !== 1) {
+      console.log(chalk.red(`ERROR: expected exactly 1 filename for ${filter} but got ${matchedFilenames.length}`));
+      console.log(chalk.red(`BUILD IS WRONG. DO NOT DEPLOY`));
+    }
+
+    // Remove the last lines from the file so it doesn't point to any source maps, and remove the sourcemap files too
+    shell.exec(`sed -i '' -e '$ d' ${matchedFilenames[0]}`);
+    shell.rm(`${matchedFilenames[0]}.map`);
+  }
 }
 
 // add to sentry code inspired by: https://medium.com/@vshab/create-react-app-and-sentry-cde1f15cbaa
-function runInitialBuildCommand(callback) {
+function runBuildCommand() {
   if (!RELEASE) {
     console.log(chalk.red('could not get release'));
     return;
   }
-  console.log(chalk.cyan('Running initial build command:'));
+  console.log(chalk.cyan('Running  build command:'));
   console.log(chalk.cyan(BASE_BUILD_COMMAND));
-  shell.exec(BASE_BUILD_COMMAND, function (error, stdout, stderr) {
-    getOutputHashes(callback);
-  });
+  shell.exec(BASE_BUILD_COMMAND);
 }
 
-async function uploadBuildToSentry(appHash, chunkHash, runtimeMainHash) {
-  console.log(chalk.bgCyan.whiteBright(appHash));
-  console.log(chalk.bgCyan.whiteBright(chunkHash));
-  console.log(chalk.bgCyan.whiteBright(runtimeMainHash));
-
-  if (!RELEASE) {
-    console.log(chalk.red('release is not set'));
-    return;
-  }
+async function uploadBuildToSentry() {
   const cli = new SentryCli();
   try {
     console.log(chalk.cyan('Creating sentry release ' + RELEASE));
 
-    await cli.releases.new(release);
+    await cli.releases.new(RELEASE);
     console.log(chalk.cyan('Uploading source maps'));
-    await cli.releases.uploadSourceMaps(release, {
+    await cli.releases.uploadSourceMaps(RELEASE, {
       include: ['build/static/js'],
       urlPrefix: '~/static/js',
       rewrite: false,
     });
     console.log(chalk.green('Finalizing release'));
-    await cli.releases.finalize(release);
-
-    const newBuildCommand = `GENERATE_SOURCEMAP=false ${BASE_BUILD_COMMAND}`;
-    console.log(chalk.cyan('Running build command without sourcemaps:'));
-    console.log(chalk.cyan(newBuildCommand));
-    shell.exec(newBuildCommand, function (error, stdout, stderr) {
-      getOutputHashes((hashapp, hashchunk, hashRuntimemain) => {
-        console.log(chalk.bgCyan.whiteBright(hashapp));
-        console.log(chalk.bgCyan.whiteBright(hashchunk));
-        console.log(chalk.bgCyan.whiteBright(hashRuntimemain));
-
-        if (hashapp === appHash && hashchunk === chunkHash && hashRuntimemain === runtimeMainHash) {
-          const deployCommand = chalk.green.bold.underline(`yarn deploy-${process.env.REACT_APP_ENV}`);
-          console.log(
-            chalk.green(`${process.env.REACT_APP_ENV} succesfully built. Ready for deploy with ${deployCommand}`)
-          );
-        } else {
-          console.log(
-            chalk.red(
-              `${process.env.REACT_APP_ENV} is wrong. hashes before and after sourcemaps aren't right. DO NOT DEPLOY`
-            )
-          );
-        }
-      });
-    });
+    await cli.releases.finalize(RELEASE);
   } catch (e) {
     console.error(chalk.red('Source maps uploading failed:', e));
   }
 }
 
-runInitialBuildCommand(uploadBuildToSentry);
+function removeSourceMaps() {
+  console.log(chalk.cyan('removing sourcemaps'));
+  const jsDirectory = 'build/static/js';
+  const jsFilesToFind = [
+    /build\/static\/js\/main\.(.*)\.chunk\.js$/,
+    /build\/static\/js\/2\.(.*)\.chunk\.js$/,
+    /build\/static\/js\/runtime-main\.(.*)\.js$/,
+  ];
+  const cssDirectory = 'build/static/css';
+  const cssFilesToFind = [/build\/static\/css\/main\.(.*)\.chunk\.css$/, /build\/static\/css\/2\.(.*)\.chunk\.css$/];
+  removeSourceMapsForDirectory(jsDirectory, jsFilesToFind);
+  removeSourceMapsForDirectory(cssDirectory, cssFilesToFind);
+}
+
+runBuildCommand();
+uploadBuildToSentry();
+removeSourceMaps();
