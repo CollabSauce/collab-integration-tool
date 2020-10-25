6a54ee0675a6917ea1e9dad86508a7fb33582ab9
Brandon Cooper <cooperjbrandon@gmail.com>
2020-10-24 23:15:25 -0700

    update logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -8,10 +8,6 @@ const Logo = ({ at, width, className, ...rest }) => {
     <div {...rest}>
       <div className={'d-flex flex-center mb-4'}>
         <img className="mr-2" src={logo} alt="Logo" width={width} />
-        <div className="d-flex flex-column text-sans-serif text-dark">
-          <p className="mb-0 font-weight-medium fs-3">Collab</p>
-          <p className="mb-0 font-weight-normal text-right fs--1 logo-sauce-text-auth">Sauce</p>
-        </div>
       </div>
     </div>
   );
@@ -22,6 +18,6 @@ Logo.propTypes = {
   className: PropTypes.string,
 };
 
-Logo.defaultProps = { width: 66 };
+Logo.defaultProps = { width: 145 };
 
 export default Logo;
